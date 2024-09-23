fc46b7236370901dc52f6dc7edd9538efb4e214d
Alex Popov <alexpopovdm@outlook.com>
2024-09-23 21:02:35 +0300

    make build

---
+++ index.60f9d3ae.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(t){t.preventDefault();var r=t.target;if("IMG"===r.tagName&&(r=r.closest("a")),"A"===r.tagName){var a=r.href;e.src=a}});
+//# sourceMappingURL=index.60f9d3ae.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+const largeImg = document.querySelector("#largeImg");
+const thumbs = document.querySelector("#thumbs");
+thumbs.addEventListener("click", (e)=>{
+    e.preventDefault();
+    let target = e.target;
+    if (target.tagName === "IMG") target = target.closest("a");
+    if (target.tagName === "A") {
+        const newSrc = target.href;
+        largeImg.src = newSrc;
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
